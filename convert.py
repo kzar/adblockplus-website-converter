3c9c4c419727f0d7eecdd221bfe5b981e1d586bd
Dave Barker <kzar@kzar.co.uk>
2015-04-11 13:54:44 +0100

    I don't think we need the localise_path filter at all.

--- convert.py
+++ convert.py
@@ -672,7 +672,7 @@ def process_animation(path):
     if (child.nodeType == Node.ELEMENT_NODE and
         child.tagName == "object" and
         child.hasAttribute("src")):
-      child.setAttribute("src", "{{'%s'|localise_path|inline_file}}" % child.getAttribute("src"))
+      child.setAttribute("src", "{{'%s'|inline_file}}" % child.getAttribute("src"))
 
   page_data = "template=raw\n\n" + xml_to_text(animation_data)
   target = os.path.join(output_dir, "pages", "animations", os.path.dirname(path).replace("_include", ""),
